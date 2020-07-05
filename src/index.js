c41e5b5949e15c4ffa0682aa46861e59d8e897c0
Harish Tata <harish.tata@cerner.com>
2020-07-05 17:39:12 -0500

    Changed the index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BaseProvider, LightTheme } from "baseui";
-import { Provider as StyletronProvider } from "styletron-react";
-import { Client as Styletron } from "styletron-engine-atomic";
+// import { BaseProvider, LightTheme } from "baseui";
+// import { Provider as StyletronProvider } from "styletron-react";
+// import { Client as Styletron } from "styletron-engine-atomic";
 
 
 import "./index.css";
@@ -11,7 +11,6 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./assests/font-awesome/css/all.css";
 
-const engine = new Styletron();
 
 ReactDOM.render(
 	// <StyletronProvider value={engine}>
